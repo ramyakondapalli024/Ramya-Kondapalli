bc6f00fd3049247da26b98499613e5e7357c3261
ramyakondapalli024 <74009589+ramyakondapalli024@users.noreply.github.com>
2025-03-03 23:20:20 +0530

    newPractice

---
+++ Assignment1/JavaNewPractice/MethodCalling.java
@@ -0,0 +1,12 @@
+public class MethodCalling
+{
+	static void myMethod() {
+		System.out.println("Called 'myMethod'");
+	}
+
+	public static void main(String[] args) {
+	    
+	    //calling method
+		myMethod();
+	}
+}
